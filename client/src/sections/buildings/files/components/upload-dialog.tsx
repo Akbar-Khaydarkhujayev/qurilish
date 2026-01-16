f7d7e1282c36b77cc9c1bbb9b6b0e176f7505727
Khaydarkhujaev Akbar <akbarboss91@gmail.com>
2026-01-16 12:55:42 +0500

    fix

--- client/src/sections/buildings/files/components/upload-dialog.tsx
+++ client/src/sections/buildings/files/components/upload-dialog.tsx
@@ -73,11 +73,21 @@ export const FileUploadDialog = ({ open, onClose, objectCardId }: IProps) => {
             onDrop={handleDrop}
             onDelete={handleDelete}
             accept={{
-              'application/*': [],
-              'image/*': [],
-              'text/*': [],
-              'video/*': [],
-              'audio/*': [],
+              'image/*': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp', '.svg'],
+              'application/pdf': ['.pdf'],
+              'application/msword': ['.doc'],
+              'application/vnd.openxmlformats-officedocument.wordprocessingml.document': ['.docx'],
+              'application/vnd.ms-excel': ['.xls'],
+              'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': ['.xlsx'],
+              'application/vnd.ms-powerpoint': ['.ppt'],
+              'application/vnd.openxmlformats-officedocument.presentationml.presentation': [
+                '.pptx',
+              ],
+              'text/plain': ['.txt'],
+              'text/csv': ['.csv'],
+              'application/zip': ['.zip'],
+              'application/x-rar-compressed': ['.rar'],
+              'application/x-7z-compressed': ['.7z'],
             }}
           />
 
